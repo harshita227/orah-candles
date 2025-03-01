d3df121dca0559ef44964b7b7f2107549b55d228
harshita227 <harshitawadhwa807@gmail.com>
2025-03-01 20:06:33 +0530

    t
    '

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import "./App.css";
-
+import Header from "./Components/Header/Header";
+import Banner from "./Components/Banner/Banner";
+import Products from "./Components/Products";
+import GetInsp from "./Components/GetInsp/GetInsp";
+import Social from "./Components/SocialPage/Social";
+import Footer from "./Components/Footer/Footer";
+import Productcoolase from "./Components/ProductCoola/ProductCoolase";
 function App() {
   return (
     <>
-      <h2>Hello</h2>
-      <header />
-      <main></main>
+      <Header />
+      <Banner />
+      <Products />
+      <Productcoolase />
+
+      <GetInsp />
+<Social/>
+<Footer />
     </>
   );
 }
